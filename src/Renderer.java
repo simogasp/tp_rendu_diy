755431315d06c6c63c823c072df084ee400ee3d2
Simone Gasparini <simone.gasparini@gmail.com>
2025-01-24 16:02:20 +0100

    another bda tag

--- src/Renderer.java
+++ src/Renderer.java
@@ -23,7 +23,7 @@ public class Renderer {
         mesh = new Mesh(scene.getMeshFileName());
         screen = new GraphicsWrapper(scene.getScreenW(), scene.getScreenH());
         screen.clearBuffer();
-        // ++ shader = new SimpleShader (screen);
+        //++ shader = new SimpleShader (screen);
         shader = new PainterShader(screen); // ??
         rasterizer = new Rasterizer(shader);
         // rasterizer = new PerspectiveCorrectRasterizer (shader);
