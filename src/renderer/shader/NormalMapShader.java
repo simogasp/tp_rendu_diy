a86c4d8dfb2df071fee6c02ab4fe2adf55ee3ea7
Timothé LEBOBE <timothe.lebobe@gmail.com>
2025-06-16 15:10:07 +0200

    fix: ajust shade method

--- src/renderer/shader/NormalMapShader.java
+++ src/renderer/shader/NormalMapShader.java
@@ -8,6 +8,11 @@ import renderer.algebra.Vector3;
 public class NormalMapShader extends Shader {
 
     /**
+     * Value tolerance to compare two double.
+     */
+    private static final double EPSILON = 0.0001;
+
+    /**
      * The depth buffer.
      */
     private DepthBuffer depthBuffer;
@@ -33,34 +38,33 @@ public class NormalMapShader extends Shader {
 
     @Override
     public void shade(Fragment fragment) {
-        if (this.depthBuffer.testFragment(fragment)) {
-            // compute the color of the pixel
-
-            Vector3 n = fragment.getNormal();
-            if (!(n.norm() == 1))
-                n.normalize();
-
-            // some vector has NaN value
-            if (Double.isNaN(n.getX())) {
-                n.set(0, 0);
-            }
-            if (Double.isNaN(n.getY())) {
-                n.set(1, 0);
-            }
-            if (Double.isNaN(n.getZ())) {
-                n.set(2, 0);
-            }
-
-            // transform a 3D direction in a color.
-            double r = n.getX() / 2 + 0.5;
-            double g = n.getY() / 2 + 0.5;
-            double b = n.getZ() / 2 + 0.5;
-
-            fragment.setColor(r, g, b);
-
-            screen.setPixel(fragment.getX(), fragment.getY(), fragment.getColor());
-            this.depthBuffer.writeFragment(fragment);
+        // don't shade a normal of a non-visible point
+        if (!this.depthBuffer.testFragment(fragment)) {
+            return;
         }
+        
+            
+        
+        Vector3 n = fragment.getNormal();
+        
+        // some vector has NaN value so we skip it
+        if (Double.isNaN(n.getX()) || Double.isNaN(n.getY()) || Double.isNaN(n.getZ())) {
+            return;
+        }
+        
+        // we normalize the vector to compute the color of the pixel.
+        if (Math.abs(n.norm() - 1) > EPSILON);
+        n.normalize();
+            
+        // transform a 3D direction in a color.
+        final double r = n.getX() / 2 + 0.5;
+        final double g = n.getY() / 2 + 0.5;
+        final double b = n.getZ() / 2 + 0.5;
+
+        fragment.setColor(r, g, b);
+
+        screen.setPixel(fragment.getX(), fragment.getY(), fragment.getColor());
+        this.depthBuffer.writeFragment(fragment);
     }
 
     
