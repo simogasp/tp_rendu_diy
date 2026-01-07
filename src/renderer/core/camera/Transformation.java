e0864a4cb5859b5c95b12b0e161da7d77b75d065
Simone Gasparini <simone.gasparini@gmail.com>
2026-01-07 14:04:04 +0100

    fix missing student instruction

--- src/renderer/core/camera/Transformation.java
+++ src/renderer/core/camera/Transformation.java
@@ -125,6 +125,7 @@ public class Transformation {
      */
     public Vector projectPoint(Vector p) throws SizeMismatchException {
         //++ // TODO
+        //++ Vector ps = new Vector(3);
         final Vector pe = worldToCamera.multiply(p); //<!!
         final Vector ps = calibration.multiply(projection.multiply(pe));
         ps.set(0, ps.get(0) / ps.get(2));
