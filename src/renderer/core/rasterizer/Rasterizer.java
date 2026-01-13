8e54d6fa598b09f3f9c0841ebe121d7ec3806acc
Simone Gasparini <simone.gasparini@gmail.com>
2026-01-13 22:32:26 +0100

    avoid nesting too many levels 2

--- src/renderer/core/rasterizer/Rasterizer.java
+++ src/renderer/core/rasterizer/Rasterizer.java
@@ -280,26 +280,27 @@ public class Rasterizer {
                 }
                 final Vector v = new Vector(1.0, (double) x, (double) y);
                 final Vector bar = cMat.multiply(v);
-                if ((bar.get(0) >= -EPSILON)
-                        && (bar.get(1) >= -EPSILON)
-                        && (bar.get(2) >= -EPSILON)) {
-                    for (int i = 0; i < numAttributes; i++) {
-                        // for the color attribute (index ranging from R to B)
-                        if (i >= Fragment.COLOR_R && i <= Fragment.COLOR_B) {
-                            // clamp the color between 0 and 1;
-                            fragment.setAttribute(i, MathUtils.clamp(
-                                    bar.get(0) * v1.getAttribute(i)
-                                    + bar.get(1) * v2.getAttribute(i)
-                                    + bar.get(2) * v3.getAttribute(i), 0., 1.));
-                        } else {
-                            // for all the other attributes take the interpolated value
-                            fragment.setAttribute(i, bar.get(0) * v1.getAttribute(i)
-                                    + bar.get(1) * v2.getAttribute(i)
-                                    + bar.get(2) * v3.getAttribute(i));
-                        }
-                    }
-                    shader.shade(fragment);
+                if ((bar.get(0) < -EPSILON)
+                        || (bar.get(1) < -EPSILON)
+                        || (bar.get(2) < -EPSILON)) {
+                    continue;
                 }
+                for (int i = 0; i < numAttributes; i++) {
+                    // for the color attribute (index ranging from R to B)
+                    if (i >= Fragment.COLOR_R && i <= Fragment.COLOR_B) {
+                        // clamp the color between 0 and 1;
+                        fragment.setAttribute(i, MathUtils.clamp(
+                                bar.get(0) * v1.getAttribute(i)
+                                + bar.get(1) * v2.getAttribute(i)
+                                + bar.get(2) * v3.getAttribute(i), 0., 1.));
+                    } else {
+                        // for all the other attributes take the interpolated value
+                        fragment.setAttribute(i, bar.get(0) * v1.getAttribute(i)
+                                + bar.get(1) * v2.getAttribute(i)
+                                + bar.get(2) * v3.getAttribute(i));
+                    }
+                }
+                shader.shade(fragment);
             }
         }
         //>!!
