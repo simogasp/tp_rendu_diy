33b6e078668e407083ab70eb16ea72697b21cc04
Duncan <duncan.kieliszak@gmail.com>
2026-06-08 10:27:24 +0200

    refactor: added the new classes to the generateStudent.py "files to identify"

--- generateStudent.py
+++ generateStudent.py
@@ -275,6 +275,7 @@ def main(working_dir: str, studentify_dir: str, archive_name: str, skip_cleaning
                            "src/renderer/core/mesh/Mesh.java",
                            "src/renderer/core/mesh/Texture.java",
                            "src/renderer/core/pipeline/Rasterizer.java",
+                           "src/renderer/core/pipeline/OutputMerger.java",
                            "src/renderer/core/shader/DepthBuffer.java",
                            "src/renderer/core/shader/TextureShader.java",
                            "src/renderer/core/camera/Transformation.java",
