3f788b1e9a4f8f31b666c67b260576b3e5ac3227
Duncan <duncan.kieliszak@gmail.com>
2026-06-08 10:15:53 +0200

    refactor: added an OutputMerger, which merges Fragment info and a FragmentOutput color to create the final image

---
+++ src/renderer/core/pipeline/FragmentOutput.java
@@ -0,0 +1,17 @@
+package renderer.core.pipeline;
+
+import java.awt.Color;
+
+public final class FragmentOutput {
+
+    private final Color color;
+
+    public FragmentOutput(Color color) {
+        this.color = color;
+    }
+
+    public Color color() {
+        return color;
+    }
+}
+
