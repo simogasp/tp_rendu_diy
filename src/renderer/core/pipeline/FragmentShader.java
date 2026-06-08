8e456413c4789de937955746593c3c91f10038a0
Duncan <duncan.kieliszak@gmail.com>
2026-06-08 10:17:24 +0200

    refactor: added the FragmentShader classe, it will be the interface implemented by all the shaders

---
+++ src/renderer/core/pipeline/FragmentShader.java
@@ -0,0 +1,35 @@
+package renderer.core.pipeline;
+
+import renderer.core.shader.Fragment;
+import renderer.controller.ColorMapFactory.Maps;
+
+public interface FragmentShader {
+	FragmentOutput shade(Fragment fragment);
+
+	/**
+	 * Whether this fragment shader supports a colormap.
+	 * Default: not supported.
+	 */
+	default boolean supportsColorMap() {
+		return false;
+	}
+
+	/**
+	 * Set the colormap to use. Default implementation is a no-op.
+	 * Implementations that support colormaps should override this.
+	 *
+	 * @param map the colormap enum value
+	 */
+	default void setColorMap(Maps map) {
+		// no-op
+	}
+
+	/**
+	 * Provide a depth range [near, far] to the shader. Default no-op.
+	 * Some shaders (e.g. DepthShader) need the scene depth range to map
+	 * depths to colors.
+	 */
+	default void setDepthRange(double near, double far) {
+		// no-op
+	}
+}
