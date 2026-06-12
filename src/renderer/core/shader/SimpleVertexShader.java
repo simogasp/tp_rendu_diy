1e60fffbe5df53d95e0dde9fa790ddb3a1124d83
Duncan <duncan.kieliszak@gmail.com>
2026-06-12 10:01:33 +0200

    feat: added a first very basic implementation of the VertexShader interface

---
+++ src/renderer/core/shader/SimpleVertexShader.java
@@ -0,0 +1,38 @@
+package renderer.core.shader;
+
+import renderer.algebra.Vector;
+import renderer.core.camera.Transformation;
+import renderer.core.pipeline.VertexInput;
+import renderer.core.pipeline.VertexOutput;
+import renderer.core.pipeline.VertexShader;
+
+public class SimpleVertexShader implements VertexShader {
+
+    private final Transformation xform;
+
+    public SimpleVertexShader(Transformation xform) {
+        this.xform = xform;
+    }
+
+    @Override
+    public VertexOutput shade(VertexInput in) {
+
+        VertexOutput out = new VertexOutput();
+
+        Vector pVertex = xform.projectPoint(in.position);
+
+        out.x = (int) Math.round(pVertex.get(0));
+        out.y = (int) Math.round(pVertex.get(1));
+        out.depth = pVertex.get(2);
+
+        out.worldPosition = in.position;
+        out.normal = in.normal;
+
+        out.color = in.color;
+
+        out.u = in.u;
+        out.v = in.v;
+
+        return out;
+    }
+}
