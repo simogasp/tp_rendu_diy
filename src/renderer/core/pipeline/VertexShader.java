a81c48c502b5419b726459e401cfb52883e9e6ff
Duncan <duncan.kieliszak@gmail.com>
2026-06-12 11:37:46 +0200

    clean: removed unused imports

--- src/renderer/core/pipeline/VertexShader.java
+++ src/renderer/core/pipeline/VertexShader.java
@@ -1,8 +1,5 @@
 package renderer.core.pipeline;
 
-import renderer.core.pipeline.VertexInput;
-import renderer.core.pipeline.VertexOutput;
-
 public interface VertexShader {
     
     VertexOutput shade(VertexInput vertex);
