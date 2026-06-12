2f63c305695fed7eb91320490a0e17193e130e7c
Duncan <duncan.kieliszak@gmail.com>
2026-06-12 11:33:27 +0200

    fix: fixed tests by changing the depth test to be strict

--- src/renderer/core/shader/DepthBuffer.java
+++ src/renderer/core/shader/DepthBuffer.java
@@ -51,7 +51,7 @@ public class DepthBuffer {
      */
     public boolean testFragment(Fragment f) {
         if (isWithinBounds(f)) {
-            return (buffer.get(f.getY(), f.getX()) >= f.getDepth()); //++ // TODO
+            return (buffer.get(f.getY(), f.getX()) > f.getDepth()); //++ // TODO
             //++ return true;
         } else {
             return false;
