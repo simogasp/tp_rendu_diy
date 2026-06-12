6c11959c801ecc411e7751d3c2ed6eedad5b1d61
Duncan <duncan.kieliszak@gmail.com>
2026-06-12 11:32:59 +0200

    feat: added constructors and a clone() method to the VertexOutput class

--- src/renderer/core/pipeline/VertexOutput.java
+++ src/renderer/core/pipeline/VertexOutput.java
@@ -10,6 +10,50 @@ public class VertexOutput {
     public Vector normal;
     public Vector worldPosition;
 
-    public double[] color = new double[4];
+    public double[] color = new double[3];
+    public double alpha;
     public double u, v;
+
+    public VertexOutput() {}
+
+    public VertexOutput(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public VertexOutput(int x, int y, double depth) {
+        this(x, y);
+        this.depth = depth;
+    }
+
+    public VertexOutput(int x, int y, double depth, Vector normal, Vector worldPos) {
+        this(x, y, depth);
+        this.normal = new Vector(normal);
+        this.worldPosition = new Vector(worldPos);
+    }
+
+    public VertexOutput(int x, int y, double depth, 
+                        Vector normal, Vector worldPos,
+                        double[] color, double alpha) {
+        this(x, y, depth, normal, worldPos);
+        this.color[0] = color[0];
+        this.color[1] = color[1];
+        this.color[2] = color[2];
+        this.alpha = alpha;
+    }
+
+    public VertexOutput(int x, int y, double depth, 
+                        Vector normal, Vector worldPos,
+                        double[] color, double alpha,
+                        double u, double v) {
+        this(x, y, depth, normal, worldPos, color, alpha);
+        this.u = u;
+        this.v = v;
+    }
+
+    public VertexOutput clone() {
+        VertexOutput clone = new VertexOutput(x, y, depth, normal, worldPosition, 
+                                              color, alpha, u, v);
+        return clone;
+    }
 }
