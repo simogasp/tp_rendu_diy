b7b15675eec117df2a1813630fd7d68fd5934174
Duncan <duncan.kieliszak@gmail.com>
2026-06-15 15:14:06 +0200

    fix: generateStudent had some old paths that do not exist anymore

--- generateStudent.py
+++ generateStudent.py
@@ -307,8 +307,8 @@ def main(working_dir: str, studentify_dir: str, archive_name: str, skip_cleaning
         
     # remove Depth and Normal shader implementations
     files_to_remove = [
-        "src/renderer/core/shader/DepthShader.java",
-        "src/renderer/core/shader/NormalMapShader.java",
+        "src/renderer/core/shader/fragmentshaders/DepthShader.java",
+        "src/renderer/core/shader/fragmentshaders/NormalMapShader.java",
         "test/unit/core/shader/DepthBufferTest.java"
     ]
     for file in files_to_remove:
