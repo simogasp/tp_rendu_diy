454c53fe04f0c075b3cfed45aae11e173a2b517b
kieliszakd <duncan.kieliszak@inp-toulouse.fr>
2026-06-18 10:15:09 +0200

    fix: removed unecessary fils_to_studentify entry

--- generateStudent.py
+++ generateStudent.py
@@ -279,7 +279,7 @@ def main(working_dir: str, studentify_dir: str, archive_name: str, skip_cleaning
                            "src/renderer/core/pipeline/OutputMerger.java",
                            "src/renderer/core/shader/DepthBuffer.java",
                            "src/renderer/core/shader/fragmentshaders/TextureShader.java",
-                           "src/renderer/core/shader/fragmentshaders/LightShader.java",
+                           # "src/renderer/core/shader/fragmentshaders/LightShader.java",
                            "src/renderer/core/camera/Transformation.java",
                            "src/renderer/core/camera/Transformation.java",
                            "src/renderer/utils/Interpolation.java",
